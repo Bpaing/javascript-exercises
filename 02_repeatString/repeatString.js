c8d59d57dbbc442b1b5c1e6a65afd068324c113d
Brendan Paing <brendanpaing@gmail.com>
2022-05-22 19:04:15 -0700

    Provide solution for all tests in 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) { return 'ERROR'; }
+    let output = '';
+    for (let i = 0; i < num; i++) {
+        let temp = output.concat(str);
+        output = temp;
+    }
+    return output;
 };
 
 // Do not edit below this line
