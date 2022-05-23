80af542501f7e3caa9a9a5382aad9c4aabe5bed4
Brendan Paing <brendanpaing@gmail.com>
2022-05-22 19:33:06 -0700

    Provide solution for all tests in 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...argsToRemove) {
+    let values = Array.from(argsToRemove);
+    let output = arr;
+    for (let i = 0; i < values.length; i++) {
+        output = output.filter(num => num !== values[i]);
+    }
+    return output;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
