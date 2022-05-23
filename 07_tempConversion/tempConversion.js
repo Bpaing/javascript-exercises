fd65862bad48b8ed01e188862340ef2c260fcc5c
Brendan Paing <brendanpaing@gmail.com>
2022-05-22 20:03:23 -0700

    Provide solution for all tests in 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  return parseFloat(((temperature - 32) * (5*1.0/9)).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  return parseFloat(((temperature * (9*1.0/5)) + 32).toFixed(1));
 };
 
 // Do not edit below this line
