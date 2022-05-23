154c379227e8f3ccfc45a7832db53c3623cbf912
Brendan Paing <brendanpaing@gmail.com>
2022-05-22 19:12:17 -0700

    Provide solution for all tests in 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split("");
+    let output = '';
+    for (let i = arr.length-1; i > -1; i--) {
+        let temp = output.concat(arr[i]);
+        output = temp;
+    }
+    return output;
 };
 
 // Do not edit below this line
