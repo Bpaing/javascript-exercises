4d4145ae2bf65ca64328e0eda6432062eb0ee95f
Brendan Paing <brendanpaing@gmail.com>
2022-05-22 19:53:21 -0700

    Provide solution for all tests in 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const ruleOne = year % 4 == 0;
+    const ruleTwo = year % 100 != 0;
+    const ruleThree = year % 400 == 0;
+    return ruleOne && (ruleTwo || ruleThree);
 };
 
 // Do not edit below this line
