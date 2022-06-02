bebd10a0a9eecce8bdf50ad1e95c7507a839c90c
Brendan Paing <brendanpaing@gmail.com>
2022-06-01 22:53:59 -0700

    Provide solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num) => {return sum + num}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, num) => {return product * num}, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  const arr = [...Array(a).keys()].map(x => ++x);
+  return multiply(arr);
 };
 
 // Do not edit below this line
