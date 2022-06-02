6833b4adb817a36670f9b82a1fc72c619256de91
Brendan Paing <brendanpaing@gmail.com>
2022-06-01 23:01:52 -0700

    Provide solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleaned = str.toLowerCase().replace(/[\W_]/g, '').split('');
+    const forward = cleaned.join();
+    const backward = cleaned.reverse().join();
+    return forward === backward;
 };
 
 // Do not edit below this line
